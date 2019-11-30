b9f802886288be69cd0e5dd9004789efc17956e0
Isaias <isaias661248@gmail.com>
2019-11-30 13:46:13 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  await Firestore.instance
+      .collection("teste")
+      .document("teste2")
+      .setData({"teste2": "teste2"});
+
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
