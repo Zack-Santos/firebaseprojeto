ddd2b89289ea349e07d7dbffce2759b2f47815cc
mouramoura <mouramoura_rm@hotmail.com>
2019-11-30 22:25:38 -0300

    filtrar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebaseflutter/telas/Faq.dart';
 import 'package:firebaseflutter/telas/addpergunta.dart';
+
 import 'package:flutter/material.dart';
 
 main() {
   runApp(MaterialApp(
-    home: AddPergunta(),
+    home: Faq(),
   ));
 }
